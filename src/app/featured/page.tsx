b1e44d8ed7cf74661ca298e948eb1354d32bdc00
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-17 12:01:46 +0500

    created testimonial section responsive

--- src/app/featured/page.tsx
+++ src/app/featured/page.tsx
@@ -1,11 +1,37 @@
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import Image from 'next/image'
 import React from 'react'
 
 const FeaturedPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='w-full h-auto flex justify-center lg:py-20 md:py-14 sm:py-10 py-6 items-center'>
+            <div className='lg:w-[85%] w-full lg:px-0 px-4 flex-col lg:gap-20 md:gap-14 sm:gap-9 gap-6 h-auto flex justify-center items-center'>
+                <h2 className='xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl text-lg font-sans uppercase'>Featured Collections</h2>
+
+                <div className='w-full flex flex-col justify-center items-center gap-16 h-auto '>
+                    <div className='relative w-full h-auto'>
+                        <div className='flex w-[835px] h-[636px] rounded-2xl bg-white drop-shadow-lg'>
+                            <div className='content w-1/2 h-full p-3 flex justify-center items-center gap-10 flex-col'>
+                                <div className='flex flex-col w-auto h-auto gap-4'>
+                                    <h2 className='xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl text-lg font-sans uppercase'>Urban Classics</h2>
+                                    <p className='md:text-base sm:text-sm text-xs font-mono lg:max-w-md md:max-w-xs sm:max-w-2xs xs:max-w-[200px] w-full'>
+                                        Modern designs with timeless appeal for everyday wear.
+                                    </p>
+                                    <div className="h-auto w-full lg:py-3 py-1 flex justify-end">
+                                        <Button className='px-6 py-4 text-base bg-transparent border border-primary text-primary rounded-none'>Add To Cart</Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='content w-1/2 h-full flex justify-center items-center '>
+                                <Image src='/images/featured-image1' alt='client-image' width={100} height={100} className='w-full h-full object-cover' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default FeaturedPage
--- src/app/testimonial/page.tsx
+++ src/app/testimonial/page.tsx
@@ -29,17 +29,18 @@ const TestimonialPage = () => {
                 <div className='w-full flex flex-col justify-center items-center gap-16 h-auto '>
                     {
                         testimonial_data.map((testimonial, index) => (
-                            <div key={index} className='flex w-full flex-row justify-between group items-center gap-3'>
+                            <div key={index} className='flex w-full h-auto max-xs:relative xs:flex-row flex-col justify-between group items-center gap-3'>
                                 <p className='lg:text-xl md:text-lg  sm:text-base xs:text-sm text-xs font-mono lg:max-w-md md:max-w-xs sm:max-w-2xs xs:max-w-[200px] w-full'>
                                     {`${testimonial.testimonial}`}
                                 </p>
-                                <div className='max-w-xs min-w-xs relative flex h-auto  justify-center items-center'>
+                                <div className='max-w-xs max-xs:hidden lg:min-w-xs md:min-w-[250px] sm:min-w-[200px] xs:min-w-[150px] w-[120px] relative flex h-auto  justify-center items-center'>
                                     <hr className='w-full  border-2 border-black' />
-                                    <div className='rounded-full group-hover:translate-x-75 duration-500 transition-transform absolute h-[100px] w-[100px]'>
+                                    <div className='rounded-full group-hover:translate-x-75 duration-500 transition-transform absolute md:h-[100px] md:w-[100px] xs:h-[75px] xs:w-[75px] h-[50px] w-[50px]'>
                                         <Image src={testimonial.image} alt='client-image' width={100} height={100} className='w-full h-full object-cover rounded-full' />
                                     </div>
                                 </div>
-                                <p className='font-mono text-2xl'>{testimonial.name}</p>
+                                <Image src={testimonial.image} alt='client-image' width={100} height={100} className='w-[50px] h-[50px] min-xs:hidden object-cover rounded-full' />
+                                <p className='font-mono xl:text-2xl md:text-xl sm:text-lg text-base'>{testimonial.name}</p>
                             </div>
                         ))
                     }
